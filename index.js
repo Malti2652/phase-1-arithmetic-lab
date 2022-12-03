525d7304bbc91c9cc871ddf17d4d53d746339020
Eddington Rowellis <eddingtonibi@gmail.com>
2022-12-03 20:34:29 +0300

    Completed arithmetic-lab assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply)
 
-// Write your code here
+let random = Math.ceil(Math.random()*10)+1;
+
+let num3 = 22;
+let num4 = 9;
+let mod = 4;
+
+let max = Math.max(20);
+
